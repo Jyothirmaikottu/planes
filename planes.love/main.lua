591ecedee40800db29296cb083bed5a25f161ee4
vihar <vihar.kurama@gmail.com>
2017-10-29 01:18:23 +0530

    Added weapons

--- planes.love/main.lua
+++ planes.love/main.lua
@@ -1,7 +1,17 @@
 debug = true
 
+canShoot = true
+canShootTimerMax = 0.2 
+canShootTimer = canShootTimerMax
+
+bulletImg = nil
+bullets = {}
+
+
 function love.load()
-	player.plane = love.graphics.newImage("assets/airplane.png")
+	player = { x = 300, y = 410, speed = 350, img = nil }
+	player.img = love.graphics.newImage("assets/airplane.png")
+	bulletImg = love.graphics.newImage('assets/bullet.png')
 end
 
 function love.update(dt)
@@ -10,17 +20,41 @@ function love.update(dt)
 	end
 
 	if love.keyboard.isDown('left','a') then
-	if player.x > 0 then -- binds us to the map
-		player.x = player.x - (player.speed*dt)
-	end
-	elseif love.keyboard.isDown('right','d') then
-		if player.x < (love.graphics.getWidth() - player.img:getWidth()) then
-			player.x = player.x + (player.speed*dt)
+		if player.x > 0 then -- restricting the map
+			player.x = player.x - (player.speed*dt)
 		end
-	end
+
+		elseif love.keyboard.isDown('right','d') then
+			if player.x < (love.graphics.getWidth() - player.img:getWidth()) then
+				player.x = player.x + (player.speed*dt)
+			end
+		end
+
+		canShootTimer = canShootTimer - (1 * dt)
+		if canShootTimer < 0 then
+			canShoot = true
+		end
+
+		if love.keyboard.isDown('space', 'rctrl', 'lctrl', 'ctrl') and canShoot then
+			newBullet = { x = player.x + (player.img:getWidth()/2), y = player.y, img = bulletImg }
+			table.insert(bullets, newBullet)
+			canShoot = false
+			canShootTimer = canShootTimerMax
+		end
+
+		for i, bullet in ipairs(bullets) do
+			bullet.y = bullet.y - (250 * dt)
+
+		  	if bullet.y < 0 then -- off screen removing bullets
+		  		table.remove(bullets, i)
+		  	end
+		end
 end
 
-function love.draw()
+function love.draw(dt)
 	love.graphics.setBackgroundColor(255,255,255)
-	love.graphics.draw(player.plane, player.x, player.y)
+	love.graphics.draw(player.img, player.x, player.y)
+	for i, bullet in ipairs(bullets) do
+		love.graphics.draw(bullet.img, bullet.x, bullet.y)
+	end
 end
